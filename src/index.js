8b9b1df531ab8c4a7ffe2665fa25cf51fe3410ba
Masaki Fukumoto <fm8624@gmail.com>
2021-12-23 07:36:04 +0900

    Add files

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { render } from 'react-dom';
 
-ReactDOM.render(
-  <React.StrictMode>
+import App from './App';
+import './App.css';
+
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+
+import { Provider } from 'react-redux';
+
+import rootReducer from './reducers';
+
+const middleWares = [thunk];
+const store = createStore(rootReducer, applyMiddleware(...middleWares));
+
+render(
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
